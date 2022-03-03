94a0897e5829b637bb10f7af879893d95b1b4961
Exitare <business.exitare@icloud.com>
2022-03-02 17:12:57 -0800

    Added jupyter notebook to git ignore.

--- src/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from args import ArgumentParser
-import pandas as pd
-
-if __name__ == "__main__":
-    args = ArgumentParser.get_args()
-    data = pd.read_csv(args.file, sep = '\t')
-    print(data)
-
