b520f98aaaad66266127d92a2085875240b0acd9
Exitare <business.exitare@icloud.com>
2022-03-11 13:11:30 -0800

    Fixed normalization.

--- run_batch.sh
+++ run_batch.sh
@@ -1,5 +1,5 @@
 
-for filename in ./data/*.csv;
+for filename in ./data/*.tsv;
 do
    source venv/bin/activate
    python3 src/main.py --file "${filename}"
--- src/main.py
+++ src/main.py
@@ -11,15 +11,20 @@ if __name__ == "__main__":
     args = ArgumentParser.get_args()
     data = pd.read_csv(args.file, sep='\t', index_col=0)
     # data = feature_selection(data)
-    normalized_data: pd.DataFrame = Preprocessing.normalize(data, args.normalization, args.feature_range)
-    X_train, X_val, X_test = create_splits(normalized_data)
+    Preprocessing.normalize(data, args.normalization, args.feature_range)
+    X_train, X_val, X_test = create_splits(data)
+
+    X_train = Preprocessing.normalize(data=X_train, method=args.normalization, feature_range=args.feature_range)
+    X_val = Preprocessing.normalize(data=X_val, method=args.normalization, feature_range=args.feature_range)
+    X_test = Preprocessing.normalize(data=X_test, method=args.normalization, feature_range=args.feature_range)
+
     print(X_train, X_val, X_test)
     input()
     model, enc, dec, hist = DRT_VAE.build_model(X_train, X_val, X_train.shape[1],
                                                 embedding_dimension=20)
     mean, var, latent_space = enc.predict(X_test)
     prediction = dec.predict(latent_space)
-    r2_scores = Evaluation.calculate_r2_score(X_test, prediction, list(normalized_data.columns))
+    r2_scores = Evaluation.calculate_r2_score(X_test, prediction, list(data.columns))
 
     file_name = Path(args.file).stem
     r2_scores.to_csv(Path("results", f"{file_name}_r2_score.csv"), index=False)
