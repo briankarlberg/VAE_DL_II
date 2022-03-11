9746dae12a8a96ffe9a034c4ca4233582d7a3bfd
Brian Karlberg <karlberb@ohsu.edu>
2022-03-11 11:04:29 -0800

    Update cell line to index
    Solve float error

--- src/main.py
+++ src/main.py
@@ -9,10 +9,12 @@ from evaluation.evaluation import Evaluation
 
 if __name__ == "__main__":
     args = ArgumentParser.get_args()
-    data = pd.read_csv(args.file, sep='\t')
-    data = feature_selection(data)
+    data = pd.read_csv(args.file, sep='\t', index_col=0)
+    # data = feature_selection(data)
     normalized_data: pd.DataFrame = Preprocessing.normalize(data, args.normalization, args.feature_range)
     X_train, X_val, X_test = create_splits(normalized_data)
+    print(X_train, X_val, X_test)
+    input()
     model, enc, dec, hist = DRT_VAE.build_model(X_train, X_val, X_train.shape[1],
                                                 embedding_dimension=20)
     mean, var, latent_space = enc.predict(X_test)
