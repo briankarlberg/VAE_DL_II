639c95b9b6f1593e5b1059981b574b48572f60a6
Exitare <business.exitare@icloud.com>
2022-03-11 15:51:45 -0800

    Create results directory if not already present.

--- src/main.py
+++ src/main.py
@@ -7,9 +7,15 @@ from vae.vae import DRT_VAE
 from pathlib import Path
 from evaluation.evaluation import Evaluation
 
+results_path = Path("results")
+
 if __name__ == "__main__":
     args = ArgumentParser.get_args()
     data = pd.read_csv(args.file, sep='\t', index_col=0)
+
+    if not results_path.exists():
+        results_path.mkdir(parents=True, exist_ok=True)
+
     # data = feature_selection(data)
     Preprocessing.normalize(data, args.normalization, args.feature_range)
     X_train, X_val, X_test = create_splits(data)
@@ -18,8 +24,6 @@ if __name__ == "__main__":
     X_val = Preprocessing.normalize(data=X_val, method=args.normalization, feature_range=args.feature_range)
     X_test = Preprocessing.normalize(data=X_test, method=args.normalization, feature_range=args.feature_range)
 
-    print(X_train, X_val, X_test)
-    input()
     model, enc, dec, hist = DRT_VAE.build_model(X_train, X_val, X_train.shape[1],
                                                 embedding_dimension=20)
     mean, var, latent_space = enc.predict(X_test)
