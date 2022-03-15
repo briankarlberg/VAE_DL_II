aa05bd8e99a98e28feb81c0ef3fc2264e282cd91
Exitare <business.exitare@icloud.com>
2022-03-15 10:15:11 -0700

    Added model selection feature
    - Added new cml argument for selecting a model
    - Added a new function for building a big model
    - Added if else construct to switch model

--- src/args.py
+++ src/args.py
@@ -15,5 +15,7 @@ class ArgumentParser:
         parser.add_argument("--feature_range", "-fr", action="store", required=False,
                             help="The feature range to use for normalization. "
                                  "If normalization method selected does not use a feature range, this is ignored")
+        parser.add_argument("--model", "-m", action="store", required=False,
+                            help="Which models should be used", default="sm", choices=["bm", "sm"])
 
         return parser.parse_args()
