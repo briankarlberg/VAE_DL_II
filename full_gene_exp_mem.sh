ed3f759c9bee5cb52797226f681ea7bf1d6d4dec
Brian Karlberg <karlberb@ohsu.edu>
2022-04-08 15:58:31 -0700

    add memory option

---
+++ full_gene_exp_mem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --job-name=vae_exa_cloud.sh
+#SBATCH --time=0-24:00:00
+#SBATCH --partition=exacloud
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=./output_reports/slurm.%N.%j.out
+#SBATCH --error=./error_reports/slurm.%N.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=karlberb@ohsu.edu
+#SBATCH --mem=20G
+
+source venv/bin/activate
+python3 ./full_gene_exp.py -di $1
