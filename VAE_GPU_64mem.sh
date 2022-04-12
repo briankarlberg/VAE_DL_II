14238b3b3e61579eece9034fdae5d9502973af07
Brian Karlberg <karlberb@ohsu.edu>
2022-04-12 14:23:20 -0700

    test shells

---
+++ VAE_GPU_64mem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time 24:00:00
+#SBATCH -p gpu --gres gpu:v100:1
+#SBATCH --output=./output_reports/slurm.%N.%j.out
+#SBATCH --error=./error_reports/slurm.%N.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=karlberb@ohsu.edu
+#SBATCH --mem 64G
+
+source venv/bin/activate
+srun src/main.py --file $1
