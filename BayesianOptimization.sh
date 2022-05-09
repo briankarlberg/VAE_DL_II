b2f8f1bb3d94034af096316aefbc22643dedbc54
Exitare <business.exitare@icloud.com>
2022-05-09 15:14:37 -0700

    Fixed shell script.

--- BayesianOptimization.sh
+++ BayesianOptimization.sh
@@ -4,9 +4,9 @@
 #SBATCH -p gpu --gres gpu:v100:1
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
-#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=karlberb@ohsu.edu,kirchgae@ohsu.edu
 #SBATCH --mem 128G
 
 source venv/bin/activate
-python3 grid_search_hyper_parameter_tuning.py -cg $1 -ngc $2 -mf $3
+python3 grid_search_hyper_parameter_tuning.py -cg $1 -ncg $2 -mf $3
