1fc60f51fda0823992a1bbdd1e88cd141ce4f41e
Brian Karlberg <karlberb@ohsu.edu>
2022-05-20 15:50:05 -0700

    Reduce memory to 180G
    add venv command to readme
    add summary of 5 args to readme

--- LatentSpaceExploration.sh
+++ LatentSpaceExploration.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=karlberb@ohsu.edu,kirchgae@ohsu.edu
-#SBATCH --mem 256G
+#SBATCH --mem 180G
 
 source venv/bin/activate
 python3 latent_space_exploration.py -cg $1 -ncg $2 -mf $3 -s $4 -m $5
