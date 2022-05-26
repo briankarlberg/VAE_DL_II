1c92e1f60485df6827c483b217a3083cccbc2ce3
Exitare <business.exitare@icloud.com>
2022-05-26 10:45:40 -0700

    Added new cli arguments.

--- latent_space_exploration.py
+++ latent_space_exploration.py
@@ -8,6 +8,7 @@ from library.plotting.plots import Plotting
 from library.multi_three_encoder_vae.multi_three_encoder_architecture import MultiThreeEncoderArchitecture
 from pathlib import Path
 from library.data.folder_management import FolderManagement
+from library.regression_vae.regression_vae import RegressionVAE
 
 base_path = Path("latent_space_generation")
 
@@ -23,9 +24,13 @@ def get_args():
                         help="The file to use for non coding gene")
     parser.add_argument("-mf", "--molecular_fingerprint", action="store", required=True,
                         help="The file to use for the molecular fingerprint")
+    parser.add_argument("-r", "--response_data", action="store", required=False,
+                        help="The file containing the response data ")
+    parser.add_argument("-lt", "--latent_space", action="store", required=True,
+                        help="Defines the latent space dimensions")
     parser.add_argument("-s", "--scaling", action="store", required=False,
                         help="Which type of scaling should be used", choices=["min", "s"], default="s")
-    parser.add_argument("-m", "--model", action="store", required=False, choices=["o", "n"], default="o")
+    parser.add_argument("-m", "--model", action="store", required=False, choices=["o", "n", "r"], default="o")
     return parser.parse_args()
 
 
@@ -67,13 +72,14 @@ if __name__ == '__main__':
                                                                     features=molecular_fingerprint_data.columns.tolist(),
                                                                     method=args.scaling)
 
+    latent_space: int = args.latent_space
     amount_of_layers: dict = {
         "coding_genes": [coding_gene_data.shape[1], coding_gene_data.shape[1] / 2, coding_gene_data.shape[1] / 3,
-                         coding_gene_data.shape[1] / 4],
+                         coding_gene_data.shape[1] / 4, latent_space],
         "non_coding_genes": [non_coding_gene_data.shape[1], coding_gene_data.shape[1] / 2,
-                             coding_gene_data.shape[1] / 3, coding_gene_data.shape[1] / 4],
+                             coding_gene_data.shape[1] / 3, coding_gene_data.shape[1] / 4, latent_space],
         "molecular_fingerprint": [molecular_fingerprint_train_data.shape[1], coding_gene_data.shape[1] / 2,
-                                  coding_gene_data.shape[1] / 3, coding_gene_data.shape[1] / 4],
+                                  coding_gene_data.shape[1] / 3, coding_gene_data.shape[1] / 4, latent_space]
     }
 
     if args.model == 'o':
@@ -83,20 +89,45 @@ if __name__ == '__main__':
             validation_data=(
                 coding_gene_validation_data, non_coding_gene_validation_data, molecular_fingerprint_validation_data),
             amount_of_layers=amount_of_layers,
-            embedding_dimension=200, folder=str(base_path)
+            embedding_dimension=latent_space, folder=str(base_path)
         )
 
-    else:
+    elif args.model == 'n':
         vae: MultiThreeEncoderArchitecture = MultiThreeEncoderArchitecture()
         vae.build_three_variational_auto_encoder(
             training_data=(coding_gene_train_data, non_coding_gene_train_data, molecular_fingerprint_train_data),
             validation_data=(
                 coding_gene_validation_data, non_coding_gene_validation_data, molecular_fingerprint_validation_data),
             amount_of_layers=amount_of_layers,
-            embedding_dimension=200, folder=str(base_path)
+            embedding_dimension=latent_space, folder=str(base_path)
         )
 
         history = vae.history
+
+    elif args.model == 'r':
+
+        if args.response_data is None:
+            raise ValueError("Please provide response data, when using the regression vae")
+
+        y: pd.DataFrame = DataLoader.load_data(file_name=args.response_data)
+
+        y = Preprocessing.normalize(data=y, features=y.columns.tolist(), method=args.scaling)
+
+        amount_of_layers["decoder"] = [latent_space, latent_space]
+
+        vae: RegressionVAE = RegressionVAE()
+        vae.build_regression_vae(
+            training_data=(coding_gene_train_data, non_coding_gene_train_data, molecular_fingerprint_train_data),
+            validation_data=(
+                coding_gene_validation_data, non_coding_gene_validation_data, molecular_fingerprint_validation_data),
+            amount_of_layers=amount_of_layers,
+            embedding_dimension=latent_space, folder=str(base_path),
+            target_value=y)
+
+        history = vae.history
+
+    else:
+        raise ValueError("Please specify a model to run")
 
     plotter: Plotting = Plotting(base_path=str(base_path))
     plotter.plot_model_performance(history=history, file_name="Model History")
